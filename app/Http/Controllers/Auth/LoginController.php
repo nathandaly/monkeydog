9b5ad02d2f686883d73695bae0340106e0e38620
Nathan Daly <justlikephp@gmail.com>
2020-09-03 17:54:36 +0100

    Adding user object to inital login.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -61,6 +61,7 @@ class LoginController extends Controller
         return response()->json([
             'success' => true,
             'login_at' => new \DateTime(),
+            'user' => $this->guard()->user(),
         ]);
     }
 
